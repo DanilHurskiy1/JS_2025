95f8b4cfa0b1bf5539839fd7098bc4105f2deaea
Danil <danilhurskiy@gmail.com>
2025-01-22 19:53:49 +0200

    idk

---
+++ lesson002/js/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+const name = "Edik";
+const age = 25;
+
+console.log(name , age);
+console.log('2' + '2');
+
+alert("Hello World");
+
+const firstname = prompt("Enter your name");
+console.log(firstname);
+
+alert("Hello " + firstname + "!");
